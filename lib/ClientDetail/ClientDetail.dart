904b794c53c443908dd7d72bb9fdecd0a497a3e0
akanksha saini <akankshasaini03@gmail.com>
2019-10-01 12:26:50 +0530

    new change

--- lib/ClientDetail/ClientDetail.dart
+++ lib/ClientDetail/ClientDetail.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import '../share/appBar.dart';
 class ClientDetail extends StatelessWidget{
   String routeName='/ClientDetail';
+  String name='ankit';
+  String number='9838749';
+  String transaction='Rs.30';
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ;
+    return Scaffold(
+      appBar: MyAppBar.getAppBar(),
+      body: Container(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[Text(this.name),Text(this.number),Text(this.transaction)],
+      ),,
+      )
+    );
   }
 }
